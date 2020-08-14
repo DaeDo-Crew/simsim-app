cda17e0f55c176944e9e327994cdc6381df23225
SoonHyeong Kwon <koreanhole1@gmail.com>
2020-08-14 16:51:00 +0900

    [app] Add React import

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,6 @@
+import * as React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import * as React from "react";
 
 export default function Home() {
   return (
