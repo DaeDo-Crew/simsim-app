bb2b3016bb9f938fcf8d22d5c87c1cd38767fedc
SoonHyeong Kwon <koreanhole1@gmail.com>
2020-08-15 14:56:28 +0900

    [app] Install antd and load fonts for antd

--- App.tsx
+++ App.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+import * as React from "react";
+import * as Font from "expo-font";
 import RootNavigator from "navigators/RootNavigator";
 
 export default function App() {
+  const [loaded] = Font.useFonts({
+    antoutline: require("@ant-design/icons-react-native/fonts/antoutline.ttf"),
+    antfill: require("@ant-design/icons-react-native/fonts/antfill.ttf"),
+  });
+  if (!loaded) {
+    return null;
+  }
   return <RootNavigator />;
 }
