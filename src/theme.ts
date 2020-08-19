4887f6a7658f2d9958465b2fab0fba067bd2c7ee
SoonHyeong Kwon <koreanhole1@gmail.com>
2020-08-19 13:14:41 +0900

    [simsim-app] 색 및 버튼 radius 추가

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,7 @@
 const theme = {
   colors: {
     primary: "#0a4e9b",
+    secondary: "#cadddb",
     black: "#2b2926",
     white: "#fff",
     ligthGrey: "#f2f2f2",
@@ -8,6 +9,7 @@ const theme = {
     darkGrey: "#828282",
     green: "#27AE60",
   },
+  borderRadius: 10,
 } as const;
 
 export default theme;
