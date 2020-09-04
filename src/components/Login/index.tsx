cbd71ddfac1e174b5e6012d4f9e3054de005aa50
SoonHyeong Kwon <koreanhole1@gmail.com>
2020-09-04 15:40:05 +0900

    [simsim-app] 로그인 페이지 스타일 적용

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { useDispatch } from "react-redux";
 import AppLayout from "modules/AppLayout";
-import { ScrollView, StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import { TextareaItem } from "@ant-design/react-native";
 import { useFormik } from "formik";
 import axios from "axios";
@@ -9,7 +9,14 @@ import { setUserToken } from "./redux/actions";
 import { LoginResponse } from "./redux/types";
 import { useNavigation } from "@react-navigation/native";
 import { LOGIN_URL } from "./apiUrls";
-import { Toast, Portal, Button } from "@ant-design/react-native";
+import {
+  Toast,
+  Portal,
+  Button,
+  WingBlank,
+  WhiteSpace,
+} from "@ant-design/react-native";
+import theme from "theme";
 
 export default function Login() {
   const dispatch = useDispatch();
@@ -41,10 +48,9 @@ export default function Login() {
             })
           );
         })
-        .catch((error) => {
+        .catch(() => {
           Portal.remove(toastKey);
           Toast.fail("로그인에 실패했습니다.", 1);
-          console.log(error);
         });
     },
   });
@@ -54,29 +60,73 @@ export default function Login() {
   };
   return (
     <AppLayout>
-      <ScrollView>
+      <WingBlank>
         <View style={LoginStyles.container}>
           <TextareaItem
             onChangeText={handleChange("id")}
             value={values.id}
             placeholder="아이디"
           />
+          <WhiteSpace size="xl" />
           <TextareaItem
             onChangeText={handleChange("password")}
             value={values.password}
             placeholder="패스워드"
           />
-          {/* https://github.com/formium/formik/issues/376/#issuecomment-466964585 */}
-          <Button onPress={handleSubmit as any}>로그인</Button>
-          <Button onPress={handleSignupButtonClicked}>회원가입</Button>
+          <WhiteSpace size="xl" />
+          <View style={LoginStyles.mainButtonContainer}>
+            {/* https://github.com/formium/formik/issues/376/#issuecomment-466964585 */}
+            <Button onPress={handleSubmit as any} style={LoginStyles.button}>
+              <Text style={LoginStyles.loginButtonText}>로그인</Text>
+            </Button>
+            <WhiteSpace size="xl" />
+            <View style={LoginStyles.subButtonContainer}>
+              <Button
+                onPress={handleSignupButtonClicked}
+                style={LoginStyles.button}
+              >
+                <Text style={LoginStyles.subButtonText}>회원가입</Text>
+              </Button>
+              <Button
+                onPress={handleSignupButtonClicked}
+                style={LoginStyles.button}
+              >
+                <Text style={LoginStyles.subButtonText}>아이디 찾기</Text>
+              </Button>
+              <Button
+                onPress={handleSignupButtonClicked}
+                style={LoginStyles.button}
+              >
+                <Text style={LoginStyles.subButtonText}>비밀번호 찾기</Text>
+              </Button>
+            </View>
+          </View>
         </View>
-      </ScrollView>
+      </WingBlank>
     </AppLayout>
   );
 }
 
 const LoginStyles = StyleSheet.create({
   container: {
+    marginTop: 64,
     justifyContent: "center",
+  },
+  mainButtonContainer: {
+    alignItems: "center",
+  },
+  subButtonContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  button: {
+    borderWidth: 0,
+  },
+  loginButtonText: {
+    color: theme.colors.primary,
+    fontWeight: "bold",
+  },
+  subButtonText: {
+    color: theme.colors.darkGrey,
   },
 });
