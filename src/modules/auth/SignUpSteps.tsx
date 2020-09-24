31f1b0c1988128411c61e1ebfe2c292936d14964
SoonHyeong Kwon <koreanhole1@gmail.com>
2020-09-24 17:03:54 +0900

    [simsim-app] SignUpSteps 스타일 수정

--- src/modules/auth/SignUpSteps.tsx
+++ src/modules/auth/SignUpSteps.tsx
@@ -1,28 +1,38 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { Steps } from "@ant-design/react-native";
 
-const Step = Steps.Step;
-
 const SIGN_UP_STEPS = [
-  { title: "아이디 입력" },
-  { title: "이메일 입력" },
+  { title: "아이디 확인" },
+  { title: "이메일 인증" },
   { title: "닉네임 입력" },
 ];
 
 export default function SignUpSteps({ currentStep }: { currentStep: number }) {
   return (
-    <Steps size="small" current={currentStep} direction="horizontal">
-      {SIGN_UP_STEPS.map((item, index) => (
-        <Step
-          key={index}
-          title={
-            <View>
-              <Text>{item.title}</Text>
-            </View>
-          }
-        />
-      ))}
-    </Steps>
+    <View style={StepStyles.container}>
+      <Steps size="small" current={currentStep} direction="horizontal">
+        {SIGN_UP_STEPS.map((item, index) => (
+          <Steps.Step
+            key={index}
+            title={
+              <View>
+                <Text style={StepStyles.title}>{item.title}</Text>
+              </View>
+            }
+          />
+        ))}
+      </Steps>
+    </View>
   );
 }
+
+const StepStyles = StyleSheet.create({
+  container: {
+    margin: 32,
+    width: "100%",
+  },
+  title: {
+    fontWeight: "bold",
+  },
+});
