7d98b2a1aff2404c174786aa8c4db4bb6a0c8e90
SoonHyeong Kwon <koreanhole1@gmail.com>
2020-09-29 16:33:22 +0900

    [simsim-app] antdesign step을 react-native-step-indicator로 변경

--- src/modules/auth/SignUpSteps.tsx
+++ src/modules/auth/SignUpSteps.tsx
@@ -1,28 +1,17 @@
 import * as React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { Steps } from "@ant-design/react-native";
+import { View, StyleSheet } from "react-native";
+import StepIndicator from "react-native-step-indicator";
 
-const SIGN_UP_STEPS = [
-  { title: "아이디 확인" },
-  { title: "이메일 인증" },
-  { title: "닉네임 입력" },
-];
+const LABELS = ["아이디 확인", "이메일 인증", "닉네임 입력"];
 
 export default function SignUpSteps({ currentStep }: { currentStep: number }) {
   return (
     <View style={StepStyles.container}>
-      <Steps size="small" current={currentStep} direction="horizontal">
-        {SIGN_UP_STEPS.map((item, index) => (
-          <Steps.Step
-            key={index}
-            title={
-              <View>
-                <Text style={StepStyles.title}>{item.title}</Text>
-              </View>
-            }
-          />
-        ))}
-      </Steps>
+      <StepIndicator
+        currentPosition={currentStep}
+        labels={LABELS}
+        stepCount={LABELS.length}
+      />
     </View>
   );
 }
@@ -31,8 +20,6 @@ const StepStyles = StyleSheet.create({
   container: {
     margin: 32,
     width: "100%",
-  },
-  title: {
-    fontWeight: "bold",
+    alignSelf: "center",
   },
 });
