ec11a9adfff9b31223d031e63189b018c89eff90
SoonHyeong Kwon <koreanhole1@gmail.com>
2020-10-04 22:30:34 +0900

    [simsim-app] AntDesign carousel -> react-native-snap-carousel로 변경

--- src/components/MeetUp/MeetUpImageCarousel.tsx
+++ src/components/MeetUp/MeetUpImageCarousel.tsx
@@ -1,42 +1,36 @@
 import * as React from "react";
-import { StyleSheet, View, Image, Dimensions, Alert } from "react-native";
-import { Carousel } from "@ant-design/react-native";
-import axios from "axios";
+import { StyleSheet, View, Image, Dimensions } from "react-native";
+import Carousel, { Pagination } from "react-native-snap-carousel";
+import theme from "theme";
+
+const SCREEN_WIDTH = Dimensions.get("screen").width;
 
 type TempData = {
-  id: number;
   imageSource: string;
 };
 
 const TEMP_DATA: TempData[] = [
   {
-    id: 1,
     imageSource: "https://homepages.cae.wisc.edu/~ece533/images/airplane.png",
   },
   {
-    id: 2,
     imageSource: "http://homepages.cae.wisc.edu/~ece533/images/cat.png",
   },
   {
-    id: 3,
     imageSource: "https://homepages.cae.wisc.edu/~ece533/images/airplane.png",
   },
   {
-    id: 4,
+    imageSource: "http://homepages.cae.wisc.edu/~ece533/images/cat.png",
+  },
+  {
     imageSource: "http://homepages.cae.wisc.edu/~ece533/images/cat.png",
   },
 ];
 
 function MeetUpImage({ imageSource }: { imageSource: string }) {
   return (
-    <View style={styles.carouselItem}>
-      <Image
-        source={{
-          uri: imageSource,
-          width: Dimensions.get("screen").width,
-          height: 300,
-        }}
-      />
+    <View style={MeetUpImageStyle.itemContainer}>
+      <Image source={{ uri: imageSource }} style={MeetUpImageStyle.image} />
     </View>
   );
 }
@@ -47,35 +41,54 @@ export default function MeetUpImageCarousel() {
     setSelectedIndex(index);
   }, []);
 
+  function _renderItem({ item }: { item: TempData }) {
+    return <MeetUpImage imageSource={item.imageSource} />;
+  }
+
+  function CarouselPagination() {
+    return (
+      <Pagination
+        dotsLength={TEMP_DATA.length}
+        activeDotIndex={seletedIndex}
+        containerStyle={{ backgroundColor: theme.colors.white }}
+        dotStyle={{
+          width: 10,
+          height: 10,
+          borderRadius: 5,
+          marginHorizontal: 8,
+          backgroundColor: theme.colors.primary,
+        }}
+        dotContainerStyle={{
+          marginHorizontal: 2,
+        }}
+        inactiveDotOpacity={0.4}
+        inactiveDotScale={0.6}
+      />
+    );
+  }
 
   return (
     <View>
-      <View>
-        <Carousel
-          style={styles.carousel}
-          selectedIndex={seletedIndex}
-          infinite
-          afterChange={onHorizontalSelectedIndexChange}
-        >
-          {TEMP_DATA.map((item) => {
-            return <MeetUpImage key={item.id} imageSource={item.imageSource} />;
-          })}
-        </Carousel>
-      </View>
+      <Carousel
+        data={TEMP_DATA}
+        renderItem={_renderItem}
+        onSnapToItem={(index) => onHorizontalSelectedIndexChange(index)}
+        sliderWidth={SCREEN_WIDTH}
+        itemWidth={SCREEN_WIDTH}
+      />
+      <CarouselPagination />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  carousel: {
-    backgroundColor: "#fff",
-    width: "100%",
+const MeetUpImageStyle = StyleSheet.create({
+  itemContainer: {
+    width: SCREEN_WIDTH,
     height: 300,
   },
-  carouselItem: {
-    flexGrow: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    height: 150,
+  image: {
+    ...StyleSheet.absoluteFillObject,
+    resizeMode: "cover",
+    borderRadius: theme.borderRadius,
   },
 });
