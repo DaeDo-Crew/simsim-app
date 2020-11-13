fcbd7033685c5a0851e5c8d846ed068898818353
SoonHyeong Kwon <koreanhole1@gmail.com>
2020-11-13 18:29:13 +0900

    [app] 버튼, textInput 모듈화

---
+++ src/modules/Button.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { StyleSheet } from "react-native";
+import { Button as RnpButton } from "react-native-paper";
+import theme from "theme";
+
+type ButtonProps = {
+  type: "text" | "outlined" | "contained";
+  label: string;
+  isSubmitting?: boolean;
+  onPress: (e?: React.FormEvent<HTMLFormElement> | undefined) => void;
+};
+
+export default function Button(props: ButtonProps) {
+  const { type, label, isSubmitting, onPress } = props;
+  return (
+    <RnpButton
+      mode={type}
+      loading={isSubmitting}
+      disabled={isSubmitting}
+      contentStyle={ButtonStyles.buttonContainer}
+      labelStyle={
+        type == "contained"
+          ? ButtonStyles.containedLabelStyle
+          : ButtonStyles.textLabelStyle
+      }
+      onPress={onPress}
+    >
+      {label}
+    </RnpButton>
+  );
+}
+
+const ButtonStyles = StyleSheet.create({
+  buttonContainer: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+  containedLabelStyle: {
+    fontSize: 16,
+    fontWeight: "700",
+  },
+  textLabelStyle: {
+    letterSpacing: 3,
+    fontSize: 16,
+    color: theme.colors.darkGrey,
+  },
+});
---
+++ src/modules/TextInput.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { StyleSheet } from "react-native";
+import { TextInput as RnpTextInput } from "react-native-paper";
+import theme from "theme";
+
+type TextInputProps = {
+  onChangeText: ((text: string) => void) & Function;
+  value: string;
+  placeholder: string;
+  textContentType?: "username" | "password";
+  secureTextEntry?: boolean;
+  error?: boolean;
+};
+
+export default function TextInput(props: TextInputProps) {
+  const {
+    onChangeText,
+    value,
+    placeholder,
+    textContentType,
+    secureTextEntry,
+    error,
+  } = props;
+  return (
+    <RnpTextInput
+      mode="outlined"
+      onChangeText={onChangeText}
+      value={value}
+      placeholder={placeholder}
+      secureTextEntry={secureTextEntry}
+      textContentType={textContentType}
+      error={error}
+      style={TextInputStyles.textInput}
+    />
+  );
+}
+
+const TextInputStyles = StyleSheet.create({
+  textInput: {
+    backgroundColor: theme.colors.white,
+  },
+});
