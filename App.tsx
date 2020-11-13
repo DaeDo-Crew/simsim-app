9eb5eefc5edb4ec792277b79afaef58235fae2a1
SoonHyeong Kwon <koreanhole1@gmail.com>
2020-11-13 16:53:49 +0900

    [app] antdesign -> react native paper로 변경

--- App.tsx
+++ App.tsx
@@ -2,26 +2,26 @@ import * as React from "react";
 import { Provider as StoreProvider } from "react-redux";
 import createReduxStore from "redux/store";
 import { PersistGate } from "redux-persist/integration/react";
-import * as Font from "expo-font";
+import { Provider as PaperProvider, DefaultTheme } from "react-native-paper";
 import RootNavigator from "navigators/RootNavigator";
-import { Provider as AntdProvider } from "@ant-design/react-native";
 
 const { store, persistor } = createReduxStore();
 
 export default function App() {
-  const [loaded] = Font.useFonts({
-    antoutline: require("@ant-design/icons-react-native/fonts/antoutline.ttf"),
-    antfill: require("@ant-design/icons-react-native/fonts/antfill.ttf"),
-  });
-  if (!loaded) {
-    return null;
-  }
+  const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: "#0a4e9b",
+    },
+  };
+
   return (
     <StoreProvider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <AntdProvider>
+        <PaperProvider theme={theme}>
           <RootNavigator />
-        </AntdProvider>
+        </PaperProvider>
       </PersistGate>
     </StoreProvider>
   );
