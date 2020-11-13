02ba47f01c4e2f8058eb57bc87e3626d381244c7
SoonHyeong Kwon <koreanhole1@gmail.com>
2020-11-13 18:31:51 +0900

    [app] 아이디 찾기 페이지 삭제

--- src/components/FindId/schemas.ts
+++
@@ -1,7 +0,0 @@
-import { FindIdRequest } from "./redux/types";
-import * as yup from "yup";
-
-export const findIdRequestSchema = yup.object<FindIdRequest>({
-  insert_code: yup.string().required(),
-  insert_email: yup.string().email().required(),
-});
