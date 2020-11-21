c4dee7d2a116b43048fd027d135a43b10fb88a94
SoonHyeong Kwon <koreanhole1@gmail.com>
2020-11-21 17:40:25 +0900

    [app] CardListHeader 모듈화

---
+++ src/components/Home/CardListHeader.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { View, StyleSheet } from "react-native";
+import theme from "theme";
+import { Text } from "react-native-paper";
+import { Entypo } from "@expo/vector-icons";
+
+export default function CardListHeader({ listTitle }: { listTitle: string }) {
+  return (
+    <View style={CardListHeaderStyles.container}>
+      <View>
+        <Text style={CardListHeaderStyles.headerTitle}>{listTitle}</Text>
+      </View>
+      <View style={CardListHeaderStyles.subItemContainer}>
+        <Text>전체보기</Text>
+        <Entypo
+          name="chevron-small-right"
+          size={20}
+          color={theme.colors.darkGrey}
+        />
+      </View>
+    </View>
+  );
+}
+
+const CardListHeaderStyles = StyleSheet.create({
+  container: {
+    marginTop: 32,
+    marginHorizontal: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  headerTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  subItemContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+});
