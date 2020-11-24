3525796ff456076b548e5f7306b7753f08cc27a8
SoonHyeong Kwon <koreanhole1@gmail.com>
2020-11-24 12:08:42 +0900

    [app] header right button 추가

---
+++ src/components/Home/HeaderRightButton.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { MaterialIcons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { HeaderRightStyles } from "modules/headerRightButton/base";
+import theme from "theme";
+
+export default function HeaderRightButton() {
+  const navigation = useNavigation();
+
+  const handleClickHeaderRightButton = React.useCallback(() => {
+    navigation.navigate("MyPage");
+  }, [navigation]);
+
+  return (
+    <MaterialIcons
+      name="account-circle"
+      size={theme.size.headerIconSize}
+      onPress={handleClickHeaderRightButton}
+      color={theme.colors.black}
+      style={HeaderRightStyles.icon}
+    />
+  );
+}
