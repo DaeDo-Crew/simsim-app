dfde5ea957a4c64f7589eca7035e5394498fabbf
SoonHyeong Kwon <koreanhole1@gmail.com>
2020-11-24 21:59:09 +0900

    [app] 임시 로그아웃 버튼 삭제

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -4,62 +4,56 @@ import { useNavigation } from "@react-navigation/native";
 import AppLayout from "modules/AppLayout";
 import MeetupCardList from "./MeetupCardList";
 import ClubList from "./ClubList";
-import Button from "modules/Button";
-import { showSnackbar } from "modules/Snackbar/redux/actions";
-import { useDispatch, useSelector } from "react-redux";
-import { setUserLogout } from "components/Login/redux/actions";
-import axios from "axios";
-import { getUserToken } from "components/Login/redux/selectors";
-import { LOGOUT_URL } from "./apiUrls";
-import qs from "qs";
+// import { showSnackbar } from "modules/Snackbar/redux/actions";
+// import { useDispatch, useSelector } from "react-redux";
+// import { setUserLogout } from "components/Login/redux/actions";
+// import axios from "axios";
+// import { getUserToken } from "components/Login/redux/selectors";
+// import { LOGOUT_URL } from "./apiUrls";
+// import qs from "qs";
 import HeaderRightButton from "./HeaderRightButton";
 
 export default function Home() {
   const navigation = useNavigation();
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  const userToken = useSelector(getUserToken);
+  // const userToken = useSelector(getUserToken);
 
   React.useLayoutEffect(() => {
     navigation.setOptions({
       headerTitle: "심심했지 🙋‍♀️",
       headerTitleStyle: {
-        fontSize: 24,
+        fontSize: 20,
         fontWeight: "bold",
       },
       headerRight: () => <HeaderRightButton />,
     });
   });
 
-  const handleLogout = () => {
-    if (userToken !== null) {
-      axios
-        .post(
-          LOGOUT_URL,
-          qs.stringify({
-            access_token: userToken.accessToken,
-          })
-        )
-        .then(() => {
-          dispatch(
-            showSnackbar({ visible: true, message: "로그아웃 했습니다." })
-          );
-          dispatch(setUserLogout(null));
-        })
-        .catch(() => {
-          showSnackbar({ visible: true, message: "로그아웃에 실패 했습니다." });
-        });
-    }
-  };
+  // const handleLogout = () => {
+  //   if (userToken !== null) {
+  //     axios
+  //       .post(
+  //         LOGOUT_URL,
+  //         qs.stringify({
+  //           access_token: userToken.accessToken,
+  //         })
+  //       )
+  //       .then(() => {
+  //         dispatch(
+  //           showSnackbar({ visible: true, message: "로그아웃 했습니다." })
+  //         );
+  //         dispatch(setUserLogout(null));
+  //       })
+  //       .catch(() => {
+  //         showSnackbar({ visible: true, message: "로그아웃에 실패 했습니다." });
+  //       });
+  //   }
+  // };
 
   return (
     <AppLayout>
       <ScrollView>
-        <Button
-          type="outlined"
-          onPress={handleLogout}
-          label="임시 로그아웃 버튼"
-        />
         <MeetupCardList />
         <ClubList />
       </ScrollView>
