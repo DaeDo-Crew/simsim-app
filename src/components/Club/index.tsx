8ff5de04e9bfcb5c386826ffc8ab769da0a8be13
SoonHyeong Kwon <koreanhole1@gmail.com>
2020-12-02 11:09:46 +0900

    [app] 동아리 카드에 동아리가 주최한 모임도 표시

--- src/components/Club/index.tsx
+++ src/components/Club/index.tsx
@@ -1,9 +1,14 @@
 import * as React from "react";
-import { ScrollView, View, StyleSheet } from "react-native";
+import { ScrollView, View, StyleSheet, Alert } from "react-native";
+import { useSelector } from "react-redux";
+import { getUserToken } from "components/Login/redux/selectors";
 import AppLayout from "modules/AppLayout";
 import { useNavigation } from "@react-navigation/native";
 import ClubCard from "./ClubCard";
 import ClubNotice from "./ClubNotice";
+import { MeetUpItem } from "components/MeetUp/redux/types";
+import { axiosInstance } from "utils/axiosInstance";
+import MeetupCardList from "components/Home/MeetupCardList";
 
 type ClubProps = {
   key: string;
@@ -17,6 +22,8 @@ type ClubProps = {
 export default function Club({ route }: { route: ClubProps }) {
   const { club_id, club_name } = route.params;
   const navigation = useNavigation();
+  const token = useSelector(getUserToken);
+  const [meetupList, setMeetupList] = React.useState<MeetUpItem[]>();
 
   React.useLayoutEffect(() => {
     navigation.setOptions({
@@ -24,12 +31,45 @@ export default function Club({ route }: { route: ClubProps }) {
     });
   });
 
+  React.useEffect(() => {
+    if (token !== null) {
+      axiosInstance({
+        url: "/meeting/read/list",
+        method: "GET",
+        headers: {
+          Authorization: token.accessToken,
+        },
+        params: {
+          clubId: club_id,
+          pageScale: 10,
+          offset: 0,
+        },
+      })
+        .then((response) => {
+          setMeetupList(response.data);
+        })
+        .catch(() => {
+          Alert.alert("내가 구독한 동아리의 모임을 불러올 수 없습니다.", "", [
+            {
+              text: "확인",
+            },
+          ]);
+        });
+    }
+  }, []);
+
   return (
     <AppLayout>
       <ScrollView>
         <View style={ClubStyles.container}>
           <ClubCard clubId={club_id} />
         </View>
+        {typeof meetupList !== "undefined" && (
+          <MeetupCardList
+            title={`${club_name}이 주최한 모임`}
+            meetupList={meetupList}
+          />
+        )}
         <ClubNotice club_id={club_id} />
       </ScrollView>
     </AppLayout>
